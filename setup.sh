7f659a8a5be3b38ba651f996d07a8f63f9773f4c
hbgyeom <151603203+hbgyeom1@users.noreply.github.com>
2025-06-04 14:27:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ rm -rf ~/.config/nvim/.git
 sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 mkdir -p ~/.local/share/applications
-cp /usr/share/applications/obsidian.desktop ~/.local/share/applications/obsidian.desktop
-cp ~/Dotfiles/Configs/hypr/keybindings.conf ~/.config/hypr/keybindings.conf
-cp ~/Dotfiles/Configs/waybar/config.jsonc ~/.config/waybar/config.jsonc
+[ -f /usr/share/applications/obsidian.desktop ] && cp /usr/share/applications/obsidian.desktop ~/.local/share/applications/
+mkdir -p ~/.config/hypr ~/.config/waybar
+[ -f ~/Dotfiles/Configs/hypr/keybindings.conf ] && cp ~/Dotfiles/Configs/hypr/keybindings.conf ~/.config/hypr/
+[ -f ~/Dotfiles/Configs/waybar/config.jsonc ] && cp ~/Dotfiles/Configs/waybar/config.jsonc ~/.config/waybar/
