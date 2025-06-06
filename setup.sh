d03b6096dd9338ea1b28263b7c58c47efc07dd99
hbgyeom <151603203+hbgyeom1@users.noreply.github.com>
2025-06-06 16:50:59 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,3 @@ sudo systemctl enable --now docker
 sudo usermod -aG docker $USER
 mkdir -p ~/.local/share/applications
 [ -f /usr/share/applications/obsidian.desktop ] && cp /usr/share/applications/obsidian.desktop ~/.local/share/applications/
-mkdir -p ~/.config/hypr ~/.config/waybar
-[ -f ~/Dotfiles/Configs/hypr/keybindings.conf ] && cp ~/Dotfiles/Configs/hypr/keybindings.conf ~/.config/hypr/
-[ -f ~/Dotfiles/Configs/waybar/config.jsonc ] && cp ~/Dotfiles/Configs/waybar/config.jsonc ~/.config/waybar/
